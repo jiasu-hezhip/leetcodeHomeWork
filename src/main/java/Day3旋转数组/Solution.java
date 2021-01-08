1c82bd33ed39fcaa5783f507099d8e745d0d1bca
hzp19940519 <18665127252@163.com>
2021-01-08 10:14:29 +0800

    第三天

---
+++ src/main/java/Day3旋转数组/Solution.java
@@ -0,0 +1,38 @@
+package Day3旋转数组;
+
+/**
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ * 输入: [1,2,3,4,5,6,7] 和 k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ *
+ * 要求使用空间复杂度为 O(1) 的 原地 算法。
+ *
+ *
+ * 先将所有元素翻转，这样尾部的 k mod n 个元素就被移至数组头部，
+ * 然后我们再翻转 [0, k mod n-1]区间的元素和 [k mod n, n-1]区间的元素。
+ *
+ */
+
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
+        }
+    }
+}
